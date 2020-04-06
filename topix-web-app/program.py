52bff899d2056d3c1e17097986c62acb8851aac7
lior <lior>
2020-04-06 18:05:46 +0300

    web app

--- topix-web-app/program.py
+++ topix-web-app/program.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
+from core import TopicModeler
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+topic_modeler = TopicModeler(None, None) # JUST AS MOCK! WILL GIVE ERROR BECAUSE OF NONES!
+
+
+@app.route('/topic_model', methods=['POST'])
+def topic_model():
+    content = request.json
+    topics = topic_modeler.topic_models(content)
+    return jsonify(topics)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
